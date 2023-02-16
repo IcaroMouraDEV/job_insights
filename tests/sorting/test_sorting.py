4247e1aab66f21a920f99aead064efaa6101fcbd
Icaro Joel <icaromoura.programador@gmail.com>
2023-02-16 17:53:05 -0300

    fix: lint

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,9 +1,10 @@
 from src.pre_built.sorting import sort_by
 from src.insights.jobs import read
 
+
 def test_sort_by_criteria():
     jobs = read('tests/mocks/jobs_sorting.csv')
-    
+
     sort_by(jobs, criteria='min_salary')
 
     assert jobs[0]['min_salary'] == '500'
