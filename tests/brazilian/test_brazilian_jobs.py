f2897e7d742288c325f40ec8e51d035ff5b33d8a
Icaro Joel <icaromoura.programador@gmail.com>
2023-02-16 17:19:07 -0300

    feat: implement test_brazilean_jobs

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,8 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    result = read_brazilian_file('tests/mocks/brazilians_jobs.csv')
+    assert 'title' in result[0]
+    assert 'salary' in result[0]
+    assert 'type' in result[0]
