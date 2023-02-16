4d810d81e00e54dfdaf6338e561e1e6ee6f86556
Icaro Joel <icaromoura.programador@gmail.com>
2023-02-16 18:23:14 -0300

    feat: implement test_sorting.py

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,11 +1,122 @@
 from src.pre_built.sorting import sort_by
-from src.insights.jobs import read
 
 
 def test_sort_by_criteria():
-    jobs = read('tests/mocks/jobs_sorting.csv')
+    jobs = [
+        {
+            'title': 'Back end developer',
+            'min_salary': 3000,
+            'max_salary': 4000,
+            'date_posted': '2022-04-12',
+        },
+        {
+            'title': 'Front end developer',
+            'min_salary': 1000,
+            'max_salary': 5000,
+            'date_posted': '2022-08-12',
+        },
+        {
+            'title': 'Web developer',
+            'min_salary': 500,
+            'max_salary': 1500,
+            'date_posted': '2022-09-12',
+        },
+        {
+            'title': 'Full stack developer',
+            'min_salary': 4000,
+            'max_salary': 8000,
+            'date_posted': '2022-12-12',
+        },
+    ]
+    expected_result = [
+        {
+            'title': 'Web developer',
+            'min_salary': 500,
+            'max_salary': 1500,
+            'date_posted': '2022-09-12',
+        },
+        {
+            'title': 'Front end developer',
+            'min_salary': 1000,
+            'max_salary': 5000,
+            'date_posted': '2022-08-12',
+        },
+        {
+            'title': 'Back end developer',
+            'min_salary': 3000,
+            'max_salary': 4000,
+            'date_posted': '2022-04-12',
+        },
+        {
+            'title': 'Full stack developer',
+            'min_salary': 4000,
+            'max_salary': 8000,
+            'date_posted': '2022-12-12',
+        },
+    ]
 
     sort_by(jobs, criteria='min_salary')
 
-    assert jobs[0]['min_salary'] == '500'
-    assert jobs[1]['min_salary'] == '1000'
+    assert jobs == expected_result
+
+    expected_result = [
+        {
+            'title': 'Full stack developer',
+            'min_salary': 4000,
+            'max_salary': 8000,
+            'date_posted': '2022-12-12',
+        },
+        {
+            'title': 'Front end developer',
+            'min_salary': 1000,
+            'max_salary': 5000,
+            'date_posted': '2022-08-12',
+        },
+        {
+            'title': 'Back end developer',
+            'min_salary': 3000,
+            'max_salary': 4000,
+            'date_posted': '2022-04-12',
+        },
+        {
+            'title': 'Web developer',
+            'min_salary': 500,
+            'max_salary': 1500,
+            'date_posted': '2022-09-12',
+        },
+    ]
+    
+    sort_by(jobs, 'max_salary')
+
+    assert jobs == expected_result
+
+    expected_result = [
+        {
+            'title': 'Full stack developer',
+            'min_salary': 4000,
+            'max_salary': 8000,
+            'date_posted': '2022-12-12',
+        },
+        {
+            'title': 'Web developer',
+            'min_salary': 500,
+            'max_salary': 1500,
+            'date_posted': '2022-09-12',
+        },
+        {
+            'title': 'Front end developer',
+            'min_salary': 1000,
+            'max_salary': 5000,
+            'date_posted': '2022-08-12',
+        },
+        {
+            'title': 'Back end developer',
+            'min_salary': 3000,
+            'max_salary': 4000,
+            'date_posted': '2022-04-12',
+        },
+    ]
+
+    sort_by(jobs, 'date_posted')
+
+    assert jobs == expected_result
