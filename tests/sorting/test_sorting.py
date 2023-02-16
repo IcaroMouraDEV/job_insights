0c67b8dbee315f7b939caaccefe53f29b0cda363
Icaro Joel <icaromoura.programador@gmail.com>
2023-02-16 17:51:33 -0300

    feat: implement test_sorting.py

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,10 @@
-# from src.pre_built.sorting import sort_by
-
+from src.pre_built.sorting import sort_by
+from src.insights.jobs import read
 
 def test_sort_by_criteria():
-    pass
+    jobs = read('tests/mocks/jobs_sorting.csv')
+    
+    sort_by(jobs, criteria='min_salary')
+
+    assert jobs[0]['min_salary'] == '500'
+    assert jobs[1]['min_salary'] == '1000'
